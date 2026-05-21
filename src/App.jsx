19a2524b90b4106e474c53fb563e9fde18fea96a
Mahalinga Raya <mahalingaraya.mr@gmail.com>
2026-05-21 13:40:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navigation } from './Navigation';
 import { Dashboard } from './Dashboard';
-import { Ecosystem } from './Ecosystem';
 import { Profile } from './Profile';
 import { LiveEngine } from './LiveEngine';
-import { LeagueOps } from './LeagueOps'; // <-- Imported
+import { LeagueOps } from './LeagueOps';
 
 export default function App() {
   return (
@@ -13,8 +12,7 @@ export default function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/tournaments" element={<Ecosystem />} />
-        <Route path="/organizer" element={<LeagueOps />} /> {/* <-- Added Route */}
+        <Route path="/organizer" element={<LeagueOps />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/live" element={<LiveEngine />} />
       </Routes>
