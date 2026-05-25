fb4b4647be8b7d3ead51410bdda0db226f2edd99
Mahalinga Raya <mahalingaraya.mr@gmail.com>
2026-05-26 02:28:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './AppContext'; // <-- Imported Global Provider
 import { Navigation } from './Navigation';
 import { Dashboard } from './Dashboard';
 import { Profile } from './Profile';
@@ -8,14 +9,16 @@ import { LeagueOps } from './LeagueOps';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/organizer" element={<LeagueOps />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/live" element={<LiveEngine />} />
-      </Routes>
-    </BrowserRouter>
+    <AppProvider> {/* Wraps entire ecosystem */}
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/organizer" element={<LeagueOps />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/live" element={<LiveEngine />} />
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
